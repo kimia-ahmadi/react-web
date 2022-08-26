7dad2ec228d0bf0393a4723a7192b0a909865ab9
kimia-ahmadi <kiimiiaahmadiii@gmail.com>
2022-08-26 21:11:06 +0430

    Updates

---
+++ precache-manifest.d9cdad71269c52e2be97b7b8c1d6c570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bfb09b3a40960c10420",
+    "url": "/react-web/static/css/main.3ffd5524.chunk.css"
+  },
+  {
+    "revision": "7bfb09b3a40960c10420",
+    "url": "/react-web/static/js/main.7bfb09b3.chunk.js"
+  },
+  {
+    "revision": "9b917095c3b8e908320a",
+    "url": "/react-web/static/js/1.9b917095.chunk.js"
+  },
+  {
+    "revision": "d7ab73ce6b5a3763b28c",
+    "url": "/react-web/static/js/runtime~main.d7ab73ce.js"
+  },
+  {
+    "revision": "8fb191c90235b9486a9dbf4545875850",
+    "url": "/react-web/static/media/video.8fb191c9.mp4"
+  },
+  {
+    "revision": "7c7140ec24a3c151fc44e5e249539c5b",
+    "url": "/react-web/static/media/logo.7c7140ec.svg"
+  },
+  {
+    "revision": "00cdb85f5a19195cb695725ea8375022",
+    "url": "/react-web/index.html"
+  }
+];
